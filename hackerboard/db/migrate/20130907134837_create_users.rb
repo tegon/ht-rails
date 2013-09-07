7354c7967c046be82856fbdac02900fb9599cad0
Tegon <leonardotegon@clickjogos.com.br>
2013-09-07 11:08:35 -0300

    database.yml no gitignore

---
+++ hackerboard/db/migrate/20130907134837_create_users.rb
@@ -0,0 +1,14 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :name, null: false
+      t.string :email, null: false
+      t.string :password_hash, null: false
+      t.string :password_salt, null: false
+
+      t.timestamps
+    end
+
+    add_index :users, :email, unique: true
+  end
+end

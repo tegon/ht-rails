da85d7ebad18c9b821f936cba2d2c61f9d273f05
Tegon <leonardotegon@clickjogos.com.br>
2013-09-08 11:39:13 -0300

    replies controller

--- hackerboard/app/controllers/replies_controller.rb
+++ hackerboard/app/controllers/replies_controller.rb
@@ -1,11 +1,18 @@
 class RepliesController < ApplicationController
+  require_logged_user
+
   def create
     @question = Question.find(params[:question_id])
     @reply = current_user.replies.new(reply_params)
     @reply.question = @question
 
-    @reply.save
-    redirect_to question_path(@question), notice: t('flash.replies.create.notice')
+    if @reply.save
+      flash[:notice] = t('flash.replies.create.notice')
+    else
+      flash[:alert] = t('flash.replies.create.alert')
+    end
+
+    redirect_to question_path(@question)
   end
 
   private
--- hackerboard/spec/integration/add_reply_spec.rb
+++ hackerboard/spec/integration/add_reply_spec.rb
@@ -3,13 +3,13 @@
 require 'spec_helper'
 
 describe 'Add reply' do
-  context 'when logged in' do
-    let!(:user) { FactoryGirl.create(:user) }
-    let!(:category) { FactoryGirl.create(:category) }
-    let!(:question) do
-      FactoryGirl.create(:question, user: user, category: category)
-    end
+  let!(:user) { FactoryGirl.create(:user) }
+  let!(:category) { FactoryGirl.create(:category) }
+  let!(:question) do
+    FactoryGirl.create(:question, user: user, category: category)
+  end
 
+  context 'when logged in' do
     context 'with valid data' do
       before do
         login with: user.email
@@ -33,7 +33,16 @@ describe 'Add reply' do
     end
 
     context 'with invalid data' do
+      before do
+        login with: user.email
+        visit question_path(question)
 
+        click_button 'Enviar resposta'
+      end
+
+      it 'displays error message' do
+        page.should have_content('Você precisa preencher sua resposta.')
+      end
     end
   end
 
@@ -44,6 +53,10 @@ describe 'Add reply' do
 
     it 'displays message' do
       page.should have_content('Você precisa efetuar o login para responder.')
+    end
+
+    it 'redirects to the question page' do
+      current_path.should eql(question_path(question))
     end
   end
 end
